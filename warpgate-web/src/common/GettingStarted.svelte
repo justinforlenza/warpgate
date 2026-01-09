43240ee38a68114cad7411db5e66508ba0eb4e42
Justin Forlenza <jforlenza@steamcenter.nyc>
2026-01-09 11:33:41 -0500

    add localstorage dismissing of getting started screen

--- warpgate-web/src/common/GettingStarted.svelte
+++ warpgate-web/src/common/GettingStarted.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
     import { faCircle } from '@fortawesome/free-regular-svg-icons'
-    import { faCircleCheck, faExternalLink } from '@fortawesome/free-solid-svg-icons'
-    import { ListGroup } from '@sveltestrap/sveltestrap'
+    import { faCircleCheck, faExternalLink, faX } from '@fortawesome/free-solid-svg-icons'
+    import { ListGroup, Button } from '@sveltestrap/sveltestrap'
     import type { SetupState } from 'gateway/lib/api'
     import Fa from 'svelte-fa'
 
     export let setupState: SetupState
+
+    let gettingStartedDismissed = localStorage.getItem('gettingStartedDismissed') === 'true'
+
+    function dismissGettingStarted() {
+        localStorage.setItem('gettingStartedDismissed', 'true')
+        gettingStartedDismissed = true
+    }
 </script>
 
+{#if !gettingStartedDismissed}
 <div class="getting-started-help border-secondary">
-    <h2>getting started</h2>
+    <div class="header">
+        <h2>getting started</h2>
+        <Button title="dismiss getting started" color="link" on:click={dismissGettingStarted}>
+            <Fa icon={faX} />
+        </Button>
+    </div>
 
     <ListGroup flush>
         <!-- eslint-disable-next-line svelte/no-target-blank -->
@@ -38,8 +51,7 @@
         </a>
     </ListGroup>
 </div>
-
-
+{/if}
 <style lang="scss">
     .getting-started-help {
         margin-bottom: 3rem;
@@ -47,10 +59,16 @@
         border-bottom: 1px solid transparent;
         padding: 1.5rem 0.5rem;
 
-        h2 {
-            font-family: 'Poppins';
-            font-weight: 700;
+        .header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
             margin-bottom: 1rem;
+
+            h2 {
+                font-family: 'Poppins';
+                font-weight: 700;
+            }
         }
 
         .item-text {
